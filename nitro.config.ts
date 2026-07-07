92d0eba624bc298f892551b86c48adab2e25e6ac
Mila <milafinance01@gmail.com>
2026-07-07 16:44:38 +0500

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -6,8 +6,5 @@ export default defineNitroConfig({
     dir: '.vercel/output',
     serverDir: '.vercel/output/functions/__server.func',
     publicDir: '.vercel/output/static'
-  },
-  runtimeConfig: {
-    // Add any runtime config here
   }
 });
